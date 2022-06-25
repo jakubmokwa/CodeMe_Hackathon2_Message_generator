c7b9105929068e85970c24fb771e8d482a012e50
Kubases <mokwajak@gmail.com>
2022-06-25 13:23:20 +0200

    project_done_commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from csv import DictReader
 date = '15.07.2022'
 
 
@@ -12,21 +13,25 @@ def read_message_file():
 
 
 def get_data():
-    """currently data typed in program, in future data will be from csv file"""
-    students_data = list()
-    tmp_data = {'names': 'James Smith', 'missing_assignments': [3, 4], 'mark': [4]}
-    students_data.append(tmp_data)
-    tmp_data = {'names': 'Basil Cooper', 'missing_assignments': [7, 8], 'mark': [4, 3, 4, 2]}
-    students_data.append(tmp_data)
-    tmp_data = {'names': 'Audrey Baldwin', 'missing_assignments': [1, 2], 'mark': [5, 4, 3, 4, 2]}
-    students_data.append(tmp_data)
-    return students_data
+    try:
+        students_data = list()
+        with open('students.csv') as f:
+            reader = DictReader(f, delimiter=';')
+            for student in reader:
+                students_data.append(student)
+        return students_data
+    except FileNotFoundError:
+        print('There is no students.csv file in directory')
+        return False
 
 
 def personalize_message(message, student):
-    current_message = message.replace('[name]', student['names'])
-    current_message = current_message.replace('[missing tasks]', str(student['missing_assignments']))
-    grade = sum(student['mark']) // len(student['mark'])
+    current_message = message.replace('[name]', student['name'])
+    current_message = current_message.replace('[surname]', student['surname'])
+    current_message = current_message.replace('[missing tasks]', str(student['missing_tasks']))
+    grade = student['marks'].split(',')
+    grade = [int(i.strip()) for i in grade]
+    grade = sum(grade) // len(grade)
     current_message = current_message.replace('[current grade]', str(grade))
     current_message = current_message.replace('[potential grade]', str(grade + 1))
     current_message = current_message.replace('[date]', date)
@@ -42,6 +47,8 @@ def write_messages(message, students_data):
 def main():
     message_text = read_message_file()
     students_data = get_data()
+    if not students_data:
+        return
     write_messages(message_text, students_data)
 
 
